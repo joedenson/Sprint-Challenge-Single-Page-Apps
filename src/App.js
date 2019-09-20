23d7c9413b3a670b742a73598a58d7c4fb6bf00f
Joe Denson <joedenson79@gmail.com>
2019-09-20 09:43:11 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import Header from "./components/Header.js";
+import CharacterList from './components/CharacterList';
+import WelcomePage from './components/WelcomePage';
 
 
 export default function App() {
   return (
     <main>
       <Header />
+      <WelcomePage />
+      <CharacterList />
+
+
+
+
+      <Route exact path="/" component={WelcomePage} />
+      <Route exact path="/character-page" component={CharacterList} />
     </main>
+
+
   );
 }
--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,25 @@
 import React from "react";
 
-export default function CharacterCard() {
-  return <span>todo: character</span>;
+const CharacterCard = props => {
+  return (
+
+    <div className="character-card">
+      <div className="character-name">
+        Name: <em>{props.name}</em>
+      </div>
+      <div className="character-status">
+        Status: <em>{props.status}</em>
+      </div>
+      <div className="character-species">
+        Species: <strong>{props.species}</strong>
+      </div>
+      <div className="character-type">
+        Type: <strong>{props.type}</strong>
+      </div>
+
+    </div>
+  )
 }
+
+
+export default CharacterCard;
